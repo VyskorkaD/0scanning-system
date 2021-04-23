613c90001d14ea44c4fa07fb23c1ca4e00afc243
d.vyskorka <52822065+VyskorkaD@users.noreply.github.com>
2021-04-23 21:11:12 +0200

    Added raports feature to raports page. Added security for raports page.

--- raports.php
+++ raports.php
@@ -2,14 +2,66 @@
     include_once 'header.php';
 ?>
 
-    <section class="buttons">
+<?php
+    if (!isset($_SESSION["userName"])) {
+        header("location: about.php");
+    }
+?>
+
+
         <div class="raport-type">
-            <button type="raport-button" name="button-1">Przejazd</button>
-            <button type="raport-button" name="button-2">Dzienny</button>
-            <button type="raport-button" name="button-3">Tygodniowy</button>
-            <button type="raport-button" name="button-4">Miesięczny</button>
+            <button onclick="expandList(this.nextSibling)" class="raport-button" type="raport-button" name="button-1">Przejazd</button>
+            <section class="raports">
+                <p>Raport przejazdu 1</p>
+                <p>Raport przejazdu 2</p>
+                <p>Raport przejazdu 3</p>
+                <p>Raport przejazdu 4</p>
+                <p>Raport przejazdu 5</p>
+                <p>Raport przejazdu 6</p>
+            </section>
+            <button onclick="expandList(this.nextSibling)" class="raport-button" type="raport-button" name="button-2">Dzienny</button>
+            <section class="raports">
+                <p>Raport dzienny 1</p>
+                <p>Raport dzienny 2</p>
+                <p>Raport dzienny 3</p>
+                <p>Raport dzienny 4</p>
+                <p>Raport dzienny 5</p>
+                <p>Raport dzienny 6</p>
+            </section>
+            <button onclick="expandList(this.nextSibling)" class="raport-button" type="raport-button" name="button-3">Tygodniowy</button>
+            <section class="raports">
+                <p>Raport tygodniowy 1</p>
+                <p>Raport tygodniowy 2</p>
+                <p>Raport tygodniowy 3</p>
+                <p>Raport tygodniowy 4</p>
+                <p>Raport tygodniowy 5</p>
+                <p>Raport tygodniowy 6</p>
+            </section>
+            <button onclick="expandList(this.nextSibling)" class="raport-button" type="raport-button" name="button-4">Miesięczny</button>
+            <section class="raports">
+                <p>Raport miesięczny 1</p>
+                <p>Raport miesięczny 2</p>
+                <p>Raport miesięczny 3</p>
+                <p>Raport miesięczny 4</p>
+                <p>Raport miesięczny 5</p>
+                <p>Raport miesięczny 6</p>
+            </section>
         </div>
-    </section>
+
+<script>
+    function expandList(element) {
+        function removeClass(element) {         //function that removes class
+            element.classList.remove("show");
+        }
+
+        let sectionArrayCollection = document.getElementsByTagName("section");  //get all hidden sections into HTMLCollection
+        let sectionArray = Array.prototype.slice.call(sectionArrayCollection); //convert it to array
+        //console.log(sectionArray);
+        sectionArray.forEach(element => removeClass(element));  //remove class "show" for every section
+
+        element.nextSibling.classList.add("show");   //add class "show" for section under clicked button
+    }
+</script>
 
 <?php
     include_once 'footer.php';
